2c74a2b186a509de09132cb8abda56d9ad67cb61
Maximiliano Aros <maxi.aros.osorio@gmail.com>
2025-02-17 22:07:32 -0300

    primeros componentes angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'first-angular-project';
+  city = 'Buenos Aires';
 }
